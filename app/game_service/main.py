913bcb8b7d1b38693fa50ba61442a6fe59b1bc14
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-09-10 11:16:14 -0300

    si incorporó el uso de pipes, para poder comunixar los 2 procesos

--- app/game_service/main.py
+++ app/game_service/main.py
@@ -4,8 +4,11 @@ from async_game_server import run_async_server
 from connect_db_server import connect_to_db
 
 if __name__ == '__main__':
-    async_server = multiprocessing.Process(target=run_async_server)
-    connect_db_server = multiprocessing.Process(target=connect_to_db)
+    async_to_db_conn1, async_to_db_conn2 = multiprocessing.Pipe()
+    db_to_async_conn1, db_to_async_conn2 = multiprocessing.Pipe()
+
+    async_server = multiprocessing.Process(target=run_async_server, args=(async_to_db_conn1, db_to_async_conn2))
+    connect_db_server = multiprocessing.Process(target=connect_to_db, args=(db_to_async_conn1, async_to_db_conn2))
 
     async_server.start()
     connect_db_server.start()
