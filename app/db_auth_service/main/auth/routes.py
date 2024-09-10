4d9ffa956873d7df630e8256fcd3e395ab260907
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-09-10 10:18:38 -0300

    respuesta de login actualizada

--- app/db_auth_service/main/auth/routes.py
+++ app/db_auth_service/main/auth/routes.py
@@ -15,7 +15,7 @@ def login():
     user = db.session.query(UserModel).filter(UserModel.username == data.get("username")).first()
 
     if user and user.validate_pass(data.get("password")):
-        return 200
+        return jsonify({'message': 'ok'}), 200
     else:
         abort(401, description="Incorrect username or password.")
     
