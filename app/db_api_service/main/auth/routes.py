1f97f21002c7623cfbd7e4936260aeb246d71617
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-09-12 15:06:05 -0300

    Actualizar respuesta de inicio de sesión para devolver el ID de usuario

--- app/db_api_service/main/auth/routes.py
+++ app/db_api_service/main/auth/routes.py
@@ -10,14 +10,18 @@ def login():
     data = request.get_json()
 
     if not data or not data.get("username") or not data.get("password"):
-        abort(400, description="Missing input parameters.")
+        return jsonify({'message': 'missing_parameters'}), 400
 
     user = db.session.query(UserModel).filter(UserModel.username == data.get("username")).first()
 
+    if not user:
+        return jsonify({'message': 'user_not_found'}), 404
+    
+    if not user.validate_pass(data.get("password")):
+        return jsonify({'message': 'incorrect_password'}), 401
+    
     if user and user.validate_pass(data.get("password")):
-        return jsonify({'user_id': user.id}), 200
-    else:
-        abort(401, description="Incorrect username or password.")
+        return jsonify({'message': 'ok', 'user_id': user.id}), 200
     
     
 @auth.route('/register' , methods=['POST'])
@@ -25,7 +29,7 @@ def register():
     user = UserModel.from_json(request.get_json())
     exists = db.session.query(UserModel).filter(UserModel.username == user.username).scalar() is not None
     if exists:
-        return 'Duplicated username', 409
+        return jsonify({'message': 'duplicated_username'}), 409
     else:
         try:
             db.session.add(user)
@@ -33,4 +37,4 @@ def register():
         except Exception as error:
             db.session.rollback()
             return str(error), 409
-        return user.to_json(), 201
+        return jsonify({'message': 'resgistered','user_id': user.id}), 201
