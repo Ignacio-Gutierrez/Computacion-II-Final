ea3b39c52290287cd23b06b3d306ea594976b43d
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-09-16 20:08:15 -0300

    Actualizar URI de la base de datos en la configuración de la aplicación

--- app/db_api_service/main/__init__.py
+++ app/db_api_service/main/__init__.py
@@ -15,7 +15,7 @@ def create_app():
         os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
     
     db.init_app(app)
 
