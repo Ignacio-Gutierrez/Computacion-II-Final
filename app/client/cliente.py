33d027f2d5c50879ed9e1a038471092e8e6fd4ba
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-09-15 22:16:28 -0300

    se corrigio condición de finalización del juego

--- app/client/cliente.py
+++ app/client/cliente.py
@@ -62,7 +62,7 @@ def telnet_client(username, password, action, ip_version):
 
                         clear_input_buffer()
 
-                        if 'ganó' in response or 'Te has retirado del juego' in response:
+                        if 'ganó' in response or 'retirado del juego' in response:
                             break
 
                         while True:
@@ -100,7 +100,7 @@ def telnet_client(username, password, action, ip_version):
 
                         clear_input_buffer()
 
-                        if 'ganó' or 'Te has retirado del juego' in response:
+                        if 'ganó' or 'retirado del juego' in response:
                             break
                         
                         while True:
